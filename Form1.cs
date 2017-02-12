7ad51c8a90ff99eaddc3c8fe2a9ea82cb372d4b8
htmlpk <htmlpk@ukr.net>
2017-02-12 20:21:08 +0200

    1e
    qw

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Красивый_запуск
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Opacity += .03;
+            if (this.Opacity == 1)
+            {
+                timer1.Stop();
+            }                
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Красивый_запуск
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form2 first = new Красивый_запуск.Form2();
+            DateTime end = DateTime.Now + TimeSpan.FromSeconds(1);
+            first.Show();
+            while (end > DateTime.Now)
+            {
+                Application.DoEvents();
+            }
+            first.Close();
+            first.Dispose();
+            Application.Run(new Form1());
+        }
+    }
+}
